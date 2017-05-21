ccfcb1483c0d1396cb575bf98dba793776255948
litaojun <78069455@qq.com>
2017-05-21 11:14:05 +0800

    反反复复

---
+++ src/student/student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+Created on May 21, 2017
+
+@author: Administrator
+'''
+#is == 区别
+import types
+def a():
+    a = "abc"
+    b = "abc"
+    c = a 
+    d = "abcd"
+    e = "abc"
+    print a==b ,type(a) is types.StringType,type(a) == type(b)
+    #a==b表示a和b指向的值相等
+    print a==c 
+    print a is b 
+    print a is c 
+    print a==d 
+    print a is d
+    print a == e
+    print a is e
+    
+if __name__ == '__main__':
+    a = 1000
+    b = 1000
+    c =  1.5
+    d = 1.5
+    print a is b,id(a),id(b)   #a is b相当于id(a) == id(b) 即a和b为同一个对象，比较结果应该为false，但由于python缓存原因导致结果为false
+    print c is d,id(c),id(d)   #详见《python核心编程》第63页 
